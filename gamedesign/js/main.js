bfb3b1669e11687af882306bf0e7781633149692
Kevin Lahoda <klahoda@binghamton.edu>
2020-05-20 15:50:53 -0400

    script dev

--- gamedesign/js/main.js
+++ gamedesign/js/main.js
@@ -1,19 +1,48 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
-
 $(document).ready(function($) {
+
+    var uri = new URI(window.location.href);
+
+    if(uri.filename() === 'game-closing.html'){
+
+        
+        var complete = uri.search(true);
+
+        console.log(complete);
+
+        'use strict';
+
+        let adventure = 0, culture = 0, food = 0;
+
+        for (const [key, value] of Object.entries(complete)) {
+            if(value === 'adventure') {
+                adventure ++;
+            } else if(value === 'food') {
+                food ++;
+            } else if(value === 'culture') {
+                culture ++;
+            }
+        }
+
+        //console.log(adventure*25);
+
+        new Chart(document.getElementById("chart"),
+            {"type":"doughnut",
+            "data":{
+                "labels":["Cutlure","Food","Adventure"],
+                "datasets":[
+                    {
+                        "label":"My First Dataset",
+                        "data":[culture*25,food*25,adventure*25],
+                        "backgroundColor":["rgb(212, 94, 127)","rgb(108, 171, 210)","rgb(255, 205, 172)"]
+                    }
+                ]
+            },
+            options: {
+                responsive: false
+            }
+        });
+    }
+
     $('a.activity img').on('mouseover', function(){
         src = $(this).attr('src').replace('.png', '-hover.gif');
         $(this).attr('src', src);
@@ -26,8 +55,6 @@ $(document).ready(function($) {
     $("a").on('click', function() {
       
         var page = $(this).attr("href");
-
-        var uri = new URI(window.location.href);
         
         if ( $(this).hasClass("romeadventure")) {
             uri.search(function(data) {
